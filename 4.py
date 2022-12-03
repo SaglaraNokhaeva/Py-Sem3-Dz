39890a1fa36d2e31c6f9927082d5a59dc9fa65ec
Saglara <nohaevass@yandex.ru>
2022-12-03 13:19:58 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -3,20 +3,12 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
-
-# a = []
-# for x in y:
-#     a.append(x)
-#     a = np.array(a)
-
 dec_number=int(input("Введите число в десятичной системе счисления: "))
 bin_number=[]
 i=0
 while dec_number>1:
     bin_number.append(dec_number%2)
-    print(bin_number[i])
-    k=int(dec_number%2)
-    dec_number=k
-    print(dec_number)
-    i+=1
-print(bin_number)
+    dec_number=int(dec_number/2)
+bin_number.append(dec_number%2)
+bin_number.reverse()
+print(*bin_number, sep="")
