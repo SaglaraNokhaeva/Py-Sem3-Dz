276e8efb6f8eedc87991696df9b4352d62e7b01c
Saglara <nohaevass@yandex.ru>
2022-12-03 13:38:33 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -9,8 +9,8 @@ Fibbonachi_list=[]
 Fibbonachi_list.append(0)
 Fibbonachi_list.append(1)
 if k>1:
-    i=1
-    while i<=k:
+    i=2
+    while i<(k+1):
         Fibbonachi_list.append(Fibbonachi_list[i-2]+Fibbonachi_list[i-1])
         i+=1
 print(Fibbonachi_list)
